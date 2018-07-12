33081522bbac495484ec7693df822c3c7451a05f
Sergey Rudenko <hzzh@outlook.com>
2018-07-12 15:55:51 +0300

    Add OOP

---
+++ OOP/OOP/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using OOP1;
+namespace OOP
+{
+    partial class Program : OOP2
+    {
+        private void StartConsole()
+        {
+            SaYHelloSergey();
+        }
+    }
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program started");
+            Program program = new Program();
+            program.StartConsole();
+
+            Console.WriteLine("Program ended");
+            Console.ReadKey();
+        }
+    }
+}
+
+namespace OOP1
+{
+    class OOP2
+    {
+        public void SaYHelloSergey()
+        {
+            Console.WriteLine("Hello Sergey!");
+        }
+    }
+}
