47f863c1d7185e0699d024b81b52e30b885985f4
Sergey Rudenko <hzzh@outlook.com>
2018-07-13 19:34:44 +0300

    New Project

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApp1
+{
+    partial class Program
+    {
+        private void StartConsole()
+        {
+            Console.WriteLine("Hello!");
+            string a = Console.ReadLine();
+            Console.WriteLine("Hello, "+a);
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program started\nPress \'e\' to Exit.\n");
+            Program program = new Program();
+            program.ExitListener();
+            program.StartConsole();
+
+
+            Console.WriteLine("\nProgram ended");
+            Console.ReadKey();
+        }
+        public void ExitListener()
+        {
+            
+        }
+    }
+}
